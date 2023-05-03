cad9c6ad6de76664d1e633c67df0b62e6e0d31a8
marcokumu <marcokumu10@gmail.com>
2023-05-03 13:09:41 +0000

    ♻️recfactor: modified the CRUD methods code

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { getGoals, updateGoal, setGoal, deleteGoal } = require('../controllers/goalController')
 
-router.get("/", (req, res) =>
-  res.status(200).json({ message: "Get Goals" })
-);
-router.post("/", (req, res) => res.status(200).json({ message: "Set Goals" }));
-
-router.put("/:id", (req, res) => res.status(200).json({ message: `Update Goal ${req.params.id}` }));
-
-router.delete("/:id", (req, res) =>
-  res.status(200).json({ message: `Delete Goal ${req.params.id}` })
-);
-
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
 
 module.exports = router;
