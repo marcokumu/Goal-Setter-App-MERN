8bde4027a452b9bddacbdf050a60802bd23e34af
marcokumu <marcokumu10@gmail.com>
2023-05-03 13:07:54 +0000

    ✨feat: added the controllers for the CRUD methods

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,34 @@
+// @desc Get  goals
+// @route GET /api/goals
+// @access Public
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get Goals" });
+}
+
+// @desc Set  goal
+// @route POST /api/goals
+// @access Private
+const setGoal = (req, res) => {
+
+  if(!req.body.text){
+    res.status(400)
+    throw new Error ('Please add a text field')
+  }
+    res.status(200).json({ message: "Set Goals" });
+}
+
+// @desc Update  goal
+// @route PUT /api/goals/:id
+// @access Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update Goal ${req.params.id}` });
+}
+
+// @desc Delete  goal
+// @route DELETE /api/goals/:id
+// @access Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+}
+
+module.exports = {getGoals, setGoal, updateGoal, deleteGoal}
